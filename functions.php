bc93b663dcd7d3634b59aa168df9cabb7a5cc997
Nahidul Hasan <nahidul.cse@gmail.com>
2018-08-28 15:23:00 +0600

    Fixed db connection

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function connectToDb(){
 
 	try {
 
-	  return new PDO('mysql:host=127.0.0.1;port=33030;dbname=simplex', 'root', 'common404');
+	  return new PDO('mysql:host=database;port=3306;dbname=simplex', 'root', 'common404');
 
 	} catch (PDOException $e) {
 
