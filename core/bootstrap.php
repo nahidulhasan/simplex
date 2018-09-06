ef814f9b37f4f7930180bcf43f8cdcfe8520baf3
Nahidul Hasan <nahidul.cse@gmail.com>
2018-09-06 14:52:43 +0600

    Removed unused code

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -5,17 +5,6 @@ $app = [];
 $app['config'] = require 'config.php';
 
 
-/*require 'core/Router.php';
-
-require 'core/Request.php';
-
-require 'core/database/Connection.php';
-
-require 'core/database/QueryBuilder.php';*/
-
-
-
-
 $app['database'] =  new QueryBuilder(
 
     Connection::make($app['config']['database'])
