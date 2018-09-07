32d32733dee8b027b93c91a8a542b714451e72a4
nahid35 <nahidul.cse@gmail.com>
2018-09-07 19:58:02 +0600

    Modify db config

--- config.php
+++ config.php
@@ -7,8 +7,8 @@ return  [
 
         'name' => 'simplex',
         'username' => 'root',
-        'password' => 'common404',
-        'connection' => 'mysql:host=database',
+        'password' => '',
+        'connection' => 'mysql:host=127.0.0.1',
         'port' => 3306,
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
@@ -17,3 +17,7 @@ return  [
 
 
 ];
+
+
+
+//$pdo = new PDO('mysql:host=127.0.0.1;dbname=simplex', 'root', '');
